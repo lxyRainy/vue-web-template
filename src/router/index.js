9582b27d57097ad3c4f962ee7798fa44a044ab41
rain <my@163.com>
2022-04-09 16:38:47 +0800

    登录

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,18 @@
 //引入vue-router路由插件
 import VueRouter from "vue-router"
-// //引入Vue
-// import Vue from "vue"
-// //使用插件
-// Vue.use(VueRouter)
 
 import Home from "@/pages/Home"
+import Login from "@/pages/Login"
 export default new VueRouter({
   // 配置路由
   routes: [
     {
       path: "/home",
       component: Home,
+    },
+    {
+      path: "/login",
+      component: Login,
     },
     // 项目一启动，立马重定向到home
     {
