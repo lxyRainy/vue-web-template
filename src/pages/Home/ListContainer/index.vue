27f16aca3ea15666098caf1c3389eb32ac37178b
rain <my@163.com>
2022-04-10 21:17:41 +0800

    首页

--- src/pages/Home/ListContainer/index.vue
+++ src/pages/Home/ListContainer/index.vue
@@ -9,7 +9,7 @@
             <div class="swiper-slide">
               <img src="./images/banner1.jpg" />
             </div>
-            <div class="swiper-slide">
+            <!-- <div class="swiper-slide">
               <img src="./images/banner2.jpg" />
             </div>
             <div class="swiper-slide">
@@ -17,7 +17,7 @@
             </div>
             <div class="swiper-slide">
               <img src="./images/banner4.jpg" />
-            </div>
+            </div> -->
           </div>
           <!-- 如果需要分页器 -->
           <div class="swiper-pagination"></div>
@@ -177,7 +177,7 @@ export default {
           width: 25%;
 
           .list-item {
-            background-image: url(../images/icons.png);
+            background-image: url(./images/icons.png);
             width: 61px;
             height: 40px;
             display: block;
--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,15 +1,22 @@
 <template>
   <div>
+    <!-- typeNav已经全局引入了 -->
     <TypeNav />
     <ListContainer />
+    <Recommend />
+    <Rank />
+    <Like />
   </div>
 </template>
 
 <script>
 import ListContainer from "@/pages/Home/ListContainer";
+import Recommend from "@/pages/Home/Recommend";
+import Rank from "@/pages/Home/Rank";
+import Like from "@/pages/Home/Like";
 export default {
   name: "Home",
-  components: [ListContainer],
+  components: { ListContainer, Recommend, Rank, Like },
 };
 </script>
 
