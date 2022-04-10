b085c7a2145a42a89eb068eb2deece207dffc3c0
rain <my@163.com>
2022-04-10 19:49:54 +0800

    banner

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,10 +1,15 @@
 <template>
-  <TypeNav />
+  <div>
+    <TypeNav />
+    <ListContainer />
+  </div>
 </template>
 
 <script>
+import ListContainer from "@/pages/Home/ListContainer";
 export default {
   name: "Home",
+  components: [ListContainer],
 };
 </script>
 
