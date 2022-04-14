e556f39a983e6a4a9f19314e6bfb5c9aa7984540
刘欣雨 <liuxinyu@atwasoft.net>
2022-04-14 18:00:45 +0800

    轮播图

--- src/pages/Home/ListContainer/index.vue
+++ src/pages/Home/ListContainer/index.vue
@@ -123,28 +123,13 @@ export default {
       bannerList: (state) => state.home.bannerList,
     }),
   },
-  mounted() {
+  mounted () {
     this.$store.dispatch("getBannerList");
   },
 };
 </script>
 
 <style scoped lang="less">
-.el-carousel__item h3 {
-  color: #475669;
-  font-size: 14px;
-  opacity: 0.75;
-  line-height: 150px;
-  margin: 0;
-}
-
-.el-carousel__item:nth-child(2n) {
-  background-color: #99a9bf;
-}
-
-.el-carousel__item:nth-child(2n + 1) {
-  background-color: #d3dce6;
-}
 .lbt_img {
   width: 100%;
   height: 100%;
